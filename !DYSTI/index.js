2b35cca29f670c7355625e5677b8ea58a2ad86e8
dunno999 <mgiancry@gmail.com>
2025-05-31 10:27:09 +0200

    Add files via upload

---
+++ !DYSTI/index.js
@@ -0,0 +1,5 @@
+let mary = document.getElementById("Mary");
+let pills = document.getElementById("Pills");
+let crystals = document.getElementById("Crystals");
+let powder = document.getElementById("Powder");
+
